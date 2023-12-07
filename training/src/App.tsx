8419229a9da8ef2894bc021fda0c98d712bf2381
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-12-07 18:15:51 -0300

    Navbar is ready and css of bottons and page is ready to

--- training/src/App.tsx
+++ training/src/App.tsx
@@ -1,8 +1,11 @@
+import Navbar from "./components/Navbar"
 import {Outlet} from "react-router-dom"
+import "./styles/App.css"
 
 export default function App(){
   return(
     <div className="App">
+      <Navbar />
       <div className="Container">
         <Outlet />
       </div>
