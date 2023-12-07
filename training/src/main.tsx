fd651f4693f28c09ae30f629460ea3e09cfc780d
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-12-07 09:03:54 -0300

    Created part of navegation da can not change Header and footer, just the components in the middle of tha page can change.

--- training/src/main.tsx
+++ training/src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import Home from './routes/Home.tsx'
+import NewPost from './routes/NewPost.tsx'
+
+// Objeto Router criado para manter o header e o footer na página, para que mude os itens do meio da página
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <NewPost />
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
